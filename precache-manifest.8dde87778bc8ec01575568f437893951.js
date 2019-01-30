880e63a5121493e13c54cfaad3d6511e93237235
Preben <181394@stud.hvl.no>
2019-01-30 18:47:14 +0100

    Updates

---
+++ precache-manifest.8dde87778bc8ec01575568f437893951.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "8a47366d02a7b9312b05",
+    "url": "/experis-workshop/static/css/main.52c1204e.chunk.css"
+  },
+  {
+    "revision": "8a47366d02a7b9312b05",
+    "url": "/experis-workshop/static/js/main.8a47366d.chunk.js"
+  },
+  {
+    "revision": "fa92c112869d3657d47b",
+    "url": "/experis-workshop/static/js/1.fa92c112.chunk.js"
+  },
+  {
+    "revision": "96c55a82d38007e17244",
+    "url": "/experis-workshop/static/js/runtime~main.96c55a82.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/experis-workshop/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "28d6d2069a458c3d267b7a476f5c08de",
+    "url": "/experis-workshop/index.html"
+  }
+];
